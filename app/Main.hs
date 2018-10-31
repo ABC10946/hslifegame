c9eb364e14e0d1d15e30144d932ec7c9cfed2da3
rect1114 <rectangle1114@gmail.com>
2018-11-01 03:55:03 +0900

    add template code for gloss

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,30 @@
 module Main where
 
-import Lib
+import Graphics.Gloss
+import Graphics.Gloss.Interface.IO.Game
+
+windowWidth, windowHeight :: Num a => a
+windowWidth = 640
+windowHeight = 480
+
+window :: Display
+window = InWindow "Lifegame in Haskell!!" (windowWidth, windowHeight) (100,100)
+
+data FieldState = FieldState
+    {
+    }
+
+initialField :: FieldState
+initialField = undefined
+
+drawField :: FieldState -> Picture
+drawField = undefined
+
+updateField :: Event -> FieldState -> FieldState
+updateField = undefined
+
+nextField :: Float -> FieldState -> FieldState
+nextField = undefined
 
 main :: IO ()
-main = someFunc
+main = play window white 30 initialField drawField updateField nextField
